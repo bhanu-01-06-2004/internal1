135d5a3cad61f7f814b55bfc3e246704d33d8547
bhanu <22h51a05k4@cmrcet.ac.in>
2024-11-27 11:03:17 +0530

    third

--- prime.java
+++ prime.java
@@ -1,6 +1,6 @@
 public class Prime {
     public static void main(String[] args) {
-        int num = 6;
+        int num = 7;
         boolean isPrime = true;
 
         for (int i = 2; i <= num / 2; i++) {
